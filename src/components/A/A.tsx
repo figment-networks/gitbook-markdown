789934f646b3c85796aeffa92811aac57d361b3f
Bartosz Hejman <bartoszhejman@gmail.com>
2021-10-28 19:10:25 +0200

    Fix a tag

--- src/components/A/A.tsx
+++ src/components/A/A.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import {StyledLink} from "./A.styles";
 
 type AProps = {
+  children: React.ReactNode & React.ReactNode[];
   href?: string;
 }
 const A = (props: AProps): JSX.Element => {
-  const {href} = props;
-  return <StyledLink href={href} target="_blank" rel="noreferrer" />
+  const {children, href} = props;
+  return <StyledLink href={href} target="_blank" rel="noreferrer">{children}</StyledLink>
 };
 
 export default A;
